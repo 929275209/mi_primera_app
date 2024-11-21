d196c428cf961565c02f39b356e015fd4cbe75fd
ABEL JURADO <abel.jurado210399@gmail.com>
2024-11-21 13:29:10 -0500

    actualizo mi primera app

--- mi_primera_app/lib/main.dart
+++ mi_primera_app/lib/main.dart
@@ -1,11 +1,41 @@
+// import 'package:flutter/material.dart';
+// import 'package:mi_primera_app/my_home_page.dart';
+// import 'package:mi_primera_app/providers/my_app_state.dart';
+// import 'package:provider/provider.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return ChangeNotifierProvider(
+//       create: (context) => MyAppState(),
+//       child: MaterialApp(
+//         title: 'My Primera App',
+//         theme: ThemeData(
+//             colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 18, 146, 252)),
+//         ),
+//         home: MyHomePage(),
+//       ),
+//     );
+//   }
+// }
+
+
 import 'package:flutter/material.dart';
+import 'package:mi_primera_app/my_app_state.dart';
 import 'package:mi_primera_app/my_home_page.dart';
-import 'package:mi_primera_app/providers/my_app_state.dart';
 import 'package:provider/provider.dart';
+
+import 'my_home_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -14,10 +44,8 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
       child: MaterialApp(
-        title: 'My Primera App',
-        theme: ThemeData(
-            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
-        ),
+        title: "Mi Primera Aplicación",
+        debugShowCheckedModeBanner: false,
         home: MyHomePage(),
       ),
     );
--- mi_primera_app/lib/my_home_page.dart
+++ mi_primera_app/lib/my_home_page.dart
@@ -1,21 +1,102 @@
+// import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+
+// import 'providers/my_app_state.dart';
+
+// class MyHomePage extends StatelessWidget {
+//   const MyHomePage({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     var appState = context.watch<MyAppState>();
+//     return Scaffold(
+//       body: Column(
+//         children: [
+//           const Text('A random idea:'),
+//           Text(appState.current.asLowerCase),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:mi_primera_app/favorites_page.dart';
+import 'package:mi_primera_app/generator_page.dart';
 
-import 'providers/my_app_state.dart';
-
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+  @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    return Scaffold(
-      body: Column(
-        children: [
-          const Text('A random idea:'),
-          Text(appState.current.asLowerCase),
-        ],
-      ),
-    );
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+      case 1:
+        page = FavoritesPage();
+      default:
+        throw UnimplementedError('No Widget for $selectedIndex');
+    }
+
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+        body: Row(
+          children: [
+            Stack(
+              children: [
+                SafeArea(
+                  child: NavigationRail(
+                    extended: constraints.maxWidth >= 600,
+                    destinations: [
+                      NavigationRailDestination(
+                        icon: Icon(Icons.home),
+                        label: Text('Inicio'),
+                      ),
+                      NavigationRailDestination(
+                        icon: Icon(Icons.favorite),
+                        label: Text('Favoritos'),
+                      ),
+                    ],
+                    selectedIndex: selectedIndex,
+                    onDestinationSelected: (value) {
+                      setState(() {
+                        selectedIndex = value;
+                      });
+                    },
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: constraints.maxWidth >= 600
+                        ? Text(
+                            '© ${DateTime.now().year} Narmeshit',
+                            style: TextStyle(fontWeight: FontWeight.w500),
+                          )
+                        : Text(
+                            '© Nms',
+                            style: TextStyle(fontWeight: FontWeight.w500),
+                          ),
+                  ),
+                ),
+              ],
+            ),
+            Expanded(
+                child: Container(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              child: page,
+            )),
+          ],
+        ),
+      );
+    });
   }
 }
